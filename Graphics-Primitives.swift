e8994f81e296b925687aa7e6123373b5e35a12d0
KholodH <91002535+KholodH@users.noreply.github.com>
2021-11-30 22:41:26 +0300

    Create Graphics-Primitives.swift

---
+++ Graphics-Primitives.swift
@@ -0,0 +1,16 @@
+
+struct Point {
+  var x: Double
+  var y: Double
+}
+
+struct Line {
+  var Start: Point
+  var End: Point
+   func length() -> Double {
+    return length
+  }
+}
+struct Triangle {
+    var Points:[point]
+}
